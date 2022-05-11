bac57c40098b30a817445049c22608c11678c958
Olga Soroka <olga.soroka@mbms.com.pl>
2022-05-11 11:50:35 +0200

    MainExtended task - typo fixed

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -2,13 +2,13 @@ public class MainExtended {
 
     public static void main(String[] args) {
         String bankName = "ING";
-        String bankAdress = "Warszawa 12345";
+        String bankAddress = "Warszawa 12345";
         boolean isItYourBank = true;
         int customerId = 12345;
         double balanceOnPrimaryAccount = 123.45;
         char sex = 'F';
         System.out.println(bankName);
-        System.out.println(bankAdress);
+        System.out.println(bankAddress);
         System.out.println(isItYourBank);
         System.out.println(customerId);
         System.out.println(balanceOnPrimaryAccount);
