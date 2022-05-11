4f8642dabbf1d43a80bc3745259fac31729a25dc
Olga Soroka <olga.soroka@mbms.com.pl>
2022-05-11 11:49:12 +0200

    MainExtended task

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended {
+
+    public static void main(String[] args) {
+        String bankName = "ING";
+        String bankAdress = "Warszawa 12345";
+        boolean isItYourBank = true;
+        int customerId = 12345;
+        double balanceOnPrimaryAccount = 123.45;
+        char sex = 'F';
+        System.out.println(bankName);
+        System.out.println(bankAdress);
+        System.out.println(isItYourBank);
+        System.out.println(customerId);
+        System.out.println(balanceOnPrimaryAccount);
+        System.out.println(sex);
+    }
+
+
+}
