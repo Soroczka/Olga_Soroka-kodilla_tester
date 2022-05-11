09022ce9c5c1499f8d07484ed935a8e4bdc57db0
Olga Soroka <olga.soroka@mbms.com.pl>
2022-05-11 11:48:46 +0200

    HelloWord class - added two additional variables

--- kodilla-intro/src/main/java/HelloWord.java
+++ kodilla-intro/src/main/java/HelloWord.java
@@ -3,13 +3,22 @@ public class HelloWord {
     private String name;
     private int age;
     private char firstLetterOfSurname;
+    private double height;
+    private boolean isGoodProgrammer;
 
     public HelloWord(String name, int age, char firstLetterOfSurname){
         this.name = name;
         this.age = age;
         this.firstLetterOfSurname = firstLetterOfSurname;
     }
-    
+
+
+    public void displayData(String imie, int age, char firstLetterOfSurname){
+        System.out.println("Hello Word!");
+        System.out.println("Mam na imię "+imie);
+        System.out.println("Mam " + age + " lat");
+        System.out.println("Pierwsza litera mojego nazwiska to: "+firstLetterOfSurname);
+    }
     public String getName() {
         return this.name;
     }
@@ -22,12 +31,7 @@ public class HelloWord {
         return this.firstLetterOfSurname;
     }
 
-    public void displayData(String imie, int age, char firstLetterOfSurname){
-        System.out.println("Hello Word!");
-        System.out.println("Mam na imię "+imie);
-        System.out.println("Mam " + age + " lat");
-        System.out.println("Pierwsza litera mojego nazwiska to: "+firstLetterOfSurname);
-    }
+
 }
 
 class AppHelloWord {
