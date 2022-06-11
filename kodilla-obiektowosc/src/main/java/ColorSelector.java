37945aa15f970ec898f707c5df69e7fd95218dee
Olga Soroka <olga.soroka@mbms.com.pl>
2022-06-11 23:17:03 +0200

    zadanie instrukcja warunkowa switch - poprawiona z wykorzystaniem enum

--- kodilla-obiektowosc/src/main/java/ColorSelector.java
+++ kodilla-obiektowosc/src/main/java/ColorSelector.java
@@ -2,39 +2,57 @@ import java.util.Scanner;
 
 public class ColorSelector {
 
-//    enum Colors {
-//        W,  /* White*/
-//        B, /* Black */
-//        O,  /* Orange */
-//        Y, /* Yellow */
-//        P, /* Purple */
-//        R, /* Red */
-//        G /* Green */
-//    }
+    private enum Colors {
+        W("White"),
+        B("Black"),
+        O("Orange"),
+        Y("Yellow"),
+        P("Purple"),
+        R("Red"),
+        G("Green");
+
+        private String customColor;
+
+        private Colors(String customColor){
+            this.customColor = customColor;
+        }
+
+        public String getCustomColor(){
+            return customColor;
+        }
+
+    }
 
     public static String getUserColor(){
         Scanner scanner = new Scanner(System.in);
+
         while (true){
             System.out.println("Select color (W - White, B - Black, O - Orange, P - Purple, R - Red, G - Green, Y - Yellow)");
             String color = scanner.nextLine().trim().toUpperCase();
-            switch (color){
-                case "W":
-                    return "White";
-                case "B":
-                    return "Black";
-                case "0":
-                    return "Orange";
-                case "P":
-                    return "Purple";
-                case "R":
-                    return  "Red";
-                case "G":
-                    return "Green";
-                case "Y":
-                    return "Yellow";
-                default:
-                    System.out.println("Wrong color name. Please try again");
+            try {
+                Colors enumColor = Colors.valueOf(color);
+                switch (enumColor){
+                    case W:
+                        return "White";
+                    case B:
+                        return "Black";
+                    case O:
+                        return "Orange";
+                    case P:
+                        return "Purple";
+                    case R:
+                        return  "Red";
+                    case G:
+                        return "Green";
+                    case Y:
+                        return "Yellow";
+                    default:
+                        System.out.println("Wrong color name. Please try again");
+                }
+            } catch (IllegalArgumentException e){
+                System.out.println("Wrong color name. Please try again");
             }
+
         }
     }
 }
