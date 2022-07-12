57471dd7f30fa8b8c4f389b3748db53d8f162a6f
Olga Soroka <olga.soroka@mbms.com.pl>
2022-07-12 23:21:19 +0200

    not finished homework

--- kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
@@ -1,4 +1,22 @@
 package com.kodilla.stream;
 
-public class UsersManagerTest {
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersManagerTest {
+    @Test
+    void testFilterChemistGroupUsernames() {
+        //Given
+        UsersRepository usersRepository = new UsersRepository();
+        List<User> chemistGroupUsernames = usersRepository.getUsersList();
+        //When
+
+        //Then
+
+    }
 }
