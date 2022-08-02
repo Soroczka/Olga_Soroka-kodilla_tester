65a5c630da1516311e34d37c5dabc2c06261390f
Olga Soroka <olga.soroka@mbms.com.pl>
2022-08-02 22:12:43 +0200

    6.3. excercise + homework

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,33 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import com.kodilla.stream.UsersRepository;
+
+import java.util.List;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        System.out.println(postsAverageForUsersOlderAndEqualThan40(UsersRepository.getUsersList()));
+        System.out.println(postsAverageForUsersYoungerThan40(UsersRepository.getUsersList()));
+    }
+
+    public static double postsAverageForUsersOlderAndEqualThan40(List<User> users){
+               double average = users
+                .stream()
+                .filter(user -> user.getAge() >= 40)
+                .mapToInt(n -> n.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        return average;
+    }
+
+    public static double postsAverageForUsersYoungerThan40(List<User> users){
+        double average = users
+                .stream()
+                .filter(user -> user.getAge() < 40)
+                .mapToInt(n -> n.getNumberOfPost())
+                .average()
+                .getAsDouble();
+        return average;
+    }
+}
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStateTest.java
@@ -0,0 +1,60 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForumStateTest {
+    
+    public static List<User> getUsersList(){
+        List<User> users = new ArrayList<>();
+        users.add(new User("Walter White", 50, 234, "Chemists"));
+        users.add(new User("Jessie Pinkman", 29, 5000, "Sales"));
+        users.add(new User("Tuco Salamanca", 34, 77, "Manager"));
+        users.add(new User("Gus Firing", 12, 5555, "Board"));
+        users.add(new User("Gale Boetticher", 44, 2, "Chemists"));
+        users.add(new User("Mike Ehrmantraut", 57, 0, "Security"));
+        return users;
+    }
+
+    @Test
+    public void testPostsAverageForUsersOlderAndEqualThan40(){
+        //Given
+        List<User> users = getUsersList();
+        double sum = 0;
+        int counter = 0;
+        for(User user: users){
+            if (user.getAge()>=40){
+                sum += user.getNumberOfPost();
+                counter++;
+            }
+        }
+        //When
+        double average = ForumStats.postsAverageForUsersOlderAndEqualThan40(users);
+        //Then
+        Assertions.assertEquals(sum/counter, average,0.001);
+    }
+
+    @Test
+    public void testPostsAverageForUsersYoungerThan40(){
+        //Given
+        List<User> users = getUsersList();
+        double sum = 0;
+        int counter = 0;
+        for(User user: users){
+            if (user.getAge()<40){
+                sum += user.getNumberOfPost();
+                counter++;
+            }
+        }
+        //When
+        double average = ForumStats.postsAverageForUsersYoungerThan40(users);
+        //Then
+        Assertions.assertEquals(sum/counter, average,0.001);
+    }
+
+}
