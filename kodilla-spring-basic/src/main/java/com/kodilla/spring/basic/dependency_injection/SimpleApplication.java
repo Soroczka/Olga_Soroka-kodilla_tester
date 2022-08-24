5bcf4599d485eca75a8343e185e9bc9726808e12
Olga Soroka <olga.soroka@mbms.com.pl>
2022-08-24 22:49:44 +0200

    9.3 - excercise

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/SimpleApplication.java
@@ -3,11 +3,13 @@ package com.kodilla.spring.basic.dependency_injection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 public class SimpleApplication {
 
-    @Autowired
-    private SkypeMessageService messageService;
+    @Resource(name = "skypeMessageService")
+    private MessageService messageService;
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
