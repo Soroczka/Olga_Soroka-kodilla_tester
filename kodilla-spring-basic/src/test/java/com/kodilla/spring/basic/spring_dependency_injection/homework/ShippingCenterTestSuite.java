64ce17f9c7b30c89df941392bd58878e5b0b8f7c
Olga Soroka <olga.soroka@mbms.com.pl>
2022-08-24 23:07:09 +0200

    9.3 - homework

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenterTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class ShippingCenterTestSuite {
+
+    @Test
+    public void shouldDisplayMessagePackageDelivered(){
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        ShippingCenter bean = context.getBean(ShippingCenter.class);
+        String message = bean.sendPackage("Test address", 15.55);
+        Assertions.assertEquals("Package delivered to: Test address", message);
+
+    }
+
+    @Test
+    public void shouldDisplayMessagePackageNotDelivered(){
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic");
+        ShippingCenter bean = context.getBean(ShippingCenter.class);
+        String message = bean.sendPackage("Test address", 33.33);
+        Assertions.assertEquals("Package not delivered to: Test address", message);
+
+    }
+}
