64549f804555c333d3fbcaf60cbf35bbf30729c2
Olga Soroka <olga.soroka@mbms.com.pl>
2022-09-08 08:23:33 +0200

    13.6 - homework part 1

--- kodilla-google-selenium/src/main/java/pages/GoogleResults.java
+++ kodilla-google-selenium/src/main/java/pages/GoogleResults.java
@@ -1,22 +1,48 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.Random;
 
 public class GoogleResults extends AbstractPage{
-    @FindBy(css = "div[class='g']")                  // [1]
+    //@FindBy(css = "div[jscontroller] > div >div > .yuRUbf > a[href]")
+    @FindBy(css = "div[jscontroller] > div >div > .yuRUbf > a[href]")
     private List<WebElement> results;
     public GoogleResults(WebDriver driver) {
         super(driver);
         PageFactory.initElements(this.driver, this);
     }
 
+
+    public static int generateRandomInt(List<WebElement> results){
+        Random random = new Random();
+        return random.nextInt(results.size());
+    }
+
     public void iSeeResults() {
         System.out.println("I see results");
         System.out.println(results.size());
+    }
+
+    public void clickAndGetRandomResult(){
+        GoogleSearch googleSearch = new GoogleSearch(driver);
+        googleSearch.searchResults();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("result-stats")));
+
+        int randomValue = generateRandomInt(results);
+        System.out.println(randomValue);
+        results.get(randomValue).click();
+    }
+
+    public List<WebElement> getResults() {
+        return results;
     }
 }
--- kodilla-google-selenium/src/test/java/TestGoogle.java
+++ kodilla-google-selenium/src/test/java/TestGoogle.java
@@ -1,28 +1,44 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.GoogleResults;
 import pages.GoogleSearch;
+
+import java.util.List;
 
 public class TestGoogle {
     WebDriver driver;
+    WebDriverWait wait;
 
-//    @Before
-//    public void testSetup() {
-//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\olga.soroka\\IdeaProjects\\kodilla-course\\chromedriver.exe");
-//        driver = new ChromeDriver();
-//        driver.navigate().to("http://wwww.google.com");
-//    }
-//
+    @Before
+    public void testSetup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\olga.soroka\\IdeaProjects\\kodilla-course\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.navigate().to("https://www.google.com");
+        driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div")).click();
+    }
+
 //    @After
 //    public void tearDown() {
 //        driver.close();
 //    }
-//
-//    @Test
-//    public void testGooglePage() {
-//        GoogleSearch googleSearch = new GoogleSearch(driver);
-//        googleSearch.searchResults();
-//    }
+
+    @Test
+    public void testGooglePage() {
+        GoogleSearch googleSearch = new GoogleSearch(driver);
+        googleSearch.searchResults();
+    }
+
+    @Test
+    public void testGoogleResult(){
+        GoogleResults googleResults = new GoogleResults(driver);
+        googleResults.clickAndGetRandomResult();
+    }
 }
