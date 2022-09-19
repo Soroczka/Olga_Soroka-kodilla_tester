4c2dec66239729d9dfcfe7bf7997aa91b29a745c
Olga Soroka <olga.soroka@mbms.com.pl>
2022-09-19 19:01:33 +0200

    14.3 homework

--- kodilla-cucumber/src/test/java/wallet/WalletSteps.java
+++ kodilla-cucumber/src/test/java/wallet/WalletSteps.java
@@ -6,19 +6,40 @@ import org.junit.Assert;
 public class WalletSteps implements En {
     private Wallet wallet = new Wallet();
     private CashSlot cashSlot = new CashSlot();
+    private int money;
+    private int amount;
+//    public WalletSteps(){
+//
+//        Given("I have deposited $200 in my wallet", () -> {
+//            wallet.deposit(200);
+//            Assert.assertEquals(200, wallet.getBalance());
+//        });
+//
+//        When("I request $30", () -> {
+//            Cashier cashier = new Cashier(cashSlot);
+//            cashier.withdraw(wallet, 30);
+//        });
+//
+//        Then("$30 should be dispensed", () -> {
+//            Assert.assertEquals(30, cashSlot.getContents());
+//        });
+//
+//    }
+
     public WalletSteps(){
-        Given("I have deposited $200 in my wallet", () -> {
-            wallet.deposit(200);
-            Assert.assertEquals(200, wallet.getBalance());
-        });
 
-        When("I request $30", () -> {
+        Given("I have deposited {int} in my wallet", (Integer int1) -> {
+            this.money = int1;
+            wallet.deposit(money);
+            Assert.assertEquals(money, wallet.getBalance());
+        });
+        When("I request {int}", (Integer int1) -> {
+            this.amount = int1;
             Cashier cashier = new Cashier(cashSlot);
-            cashier.withdraw(wallet, 30);
+            cashier.withdraw(wallet, amount);
         });
-
-        Then("$30 should be dispensed", () -> {
-            Assert.assertEquals(30, cashSlot.getContents());
+        Then("{int} should be dispensed", (Integer int1) -> {
+            Assert.assertEquals(amount, cashSlot.getContents());
         });
     }
 }
--- kodilla-cucumber/src/test/resources/wallet/cash_withdrawal.feature
+++ kodilla-cucumber/src/test/resources/wallet/cash_withdrawal.feature
@@ -1,5 +1,21 @@
 Feature: Cash Withdrawal
-  Scenario: Successful withdrawal from a wallet in credit
-    Given I have deposited $200 in my wallet
-    When I request $30
-    Then $30 should be dispensed
+#  Scenario: Successful withdrawal from a wallet in credit
+#    Given I have deposited $200 in my wallet
+#    When I request $30
+#    Then $30 should be dispensed
+
+
+  Scenario Outline: Successful withdrawal from a wallet in debit and credit
+    Given I have deposited <money> in my wallet
+    When I request <amount>
+    Then <amount> should be dispensed
+    Examples:
+      | money  | amount |
+      | 200    | 30     |
+      | 100    | 110    |
+      | 0      | 100    |
+      | 0      | -100   |
+      | -100   | 0      |
+      | -100   | 100    |
+
+
