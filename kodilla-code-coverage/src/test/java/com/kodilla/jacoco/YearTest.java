0301328a8780eb4236998eb99dec5c4fb66247fb
Olga Soroka <olga.soroka@mbms.com.pl>
2022-09-26 15:35:06 +0200

    15.2 homework

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -8,15 +8,43 @@ class YearTest {
 
     @Test
     public void shouldBeLeapYearIfDivisibleBy400() {
-
         //given
         Year year = new Year(1600);
-
         //when
         boolean isLeapYear = year.isLeap();
-
         //then
         assertTrue(isLeapYear);
     }
+
+    @Test
+    public void shouldNotBeLeapYear() {
+        //given
+        Year year = new Year(1666);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4ButNotBy100() {
+        //given
+        Year year = new Year(1440);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy4And100ButNotBy400() {
+        //given
+        Year year = new Year(1700);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertFalse(isLeapYear);
+    }
+
 
 }
